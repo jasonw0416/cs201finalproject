2c53cfcfeee6038807055a42b01af9f6bf62822e
Wonjun Lee <jasonw0416@gmail.com>
2022-10-28 00:30:59 -0700

    initial commit2

---
+++ schedulesystem/src/test/java/com/cs201finalproject/schedulesystem/SchedulesystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs201finalproject.schedulesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulesystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
