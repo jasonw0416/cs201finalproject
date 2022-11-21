c14347785130841f9ee5eb1089c0671bfba4e2c8
giozaarour <giozaarour@gmail.com>
2022-11-20 16:43:11 -0800

    db files

---
+++ database/final project.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE TrojanScheduler;
+USE TrojanScheduler;
+
+CREATE TABLE results(
+	resultsID int(11) primary key not null auto_increment, 
+    -- userID int(11) not null unique, 
+    results JSON
+    -- FOREIGN KEY fk2(userID) REFERENCES credentials(userID)
+);
+
+CREATE TABLE credentials (
+	userID int(11) primary key not null auto_increment, 
+    Username varchar(45) not null unique, 
+    Password varchar(45) not null, 
+    schedID int(11), 
+    FOREIGN KEY fk1(schedID) REFERENCES schedule(schedID)
+);
+
+CREATE TABLE professors (
+	profID int(11) primary key not null auto_increment, 
+    name varchar(45) not null unique, 
+    rating double
+);
+
+CREATE TABLE courses (
+	courseID int(11) primary key not null auto_increment, 
+    profID int(11) not null, 
+    Course_Name varchar(45) not null, 
+    GE_Category varchar(2),
+    monday TINYINT, 
+    tuesday TINYINT, 
+    wednesday TINYINT, 
+    thursday TINYINT, 
+    friday TINYINT, 
+    StartTime int not null, 
+    EndTime int not null,
+    FOREIGN KEY fk3(profID) REFERENCES professors(profID)
+);
